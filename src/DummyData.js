0391c5bf26d8077da7a2dc337b4f933ab35ea118
Norbu Tsering <ntsering91@gmail.com>
2017-11-15 00:54:58 -0500

    Adjusted ratings and added more users

--- src/DummyData.js
+++ src/DummyData.js
@@ -23,7 +23,7 @@ export var USERS =
     {"name": "Enzo Ames",
      "bio": "Founder of Plugr",
      "since": "Dec 12, 2012",
-     "rating": '2',
+     "rating": '5',
      "id": 0},
 
      {"name": "Norbu Tsering",
@@ -32,28 +32,47 @@ export var USERS =
      "rating": '5',
      "id": 1},
 
+     {"name": "Sami Abed",
+     "bio": "Hacker",
+     "since": "Nov 04, 1337",
+     "rating": '5',
+     "id": 2},
+
+     {"name": "Izidor Gertner",
+     "bio": "Retired",
+     "since": "Dec 25, 0",
+     "rating": '1',
+     "id": 3},
+
      {"name": "Rohan Swaby",
      "bio": "Database Guy",
      "since": "Apr 1, 2011",
-     'rating': '4',
-     "id": 2},],
+     'rating': '5',
+     "id": 4},],
 
   'clients': [
     {"name": "Jon Doe",
      "bio": "Lazy mofo",
      "since": "Dec 12, 2012",
      'rating': '1',
-     "id": 3},
+     "id": 100},
 
      {"name": "Alan Greenspan",
      "bio": "Dummy",
      "since": "Oct 14, 2011",
      'rating': '2',
-     "id": 4},
+     "id": 101},
 
      {"name": "Alice Cambell",
      "bio": "Soup Enthusiast",
      "since": "Apr 1, 2011",
      'rating': '3',
-     "id": 5},]
+     "id": 102},
+
+     {"name": "George Orwell",
+     "bio": "Sci-Fi Author",
+     "since": "Nov 15, 2017",
+     'rating': '5',
+     "id": 103},
+   ]
    };
